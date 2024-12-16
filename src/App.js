3e836fd240092dce9d6337056f2b55ce82d22cdb
emon <tj.emon33@gmail.com>
2024-12-16 21:02:28 +0600

    rendering a list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ export default function App() {
   );
 }
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
+];
+
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
@@ -20,7 +26,27 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>{item.quantity}</span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
