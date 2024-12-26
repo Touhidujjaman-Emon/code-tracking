623fd91a54c2bf46547dff054a2a90d2b5393746
emon <tj.emon33@gmail.com>
2024-12-26 20:12:43 +0600

    liftingup state

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ export default function App() {
   );
 }
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: true },
+// ];
 
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
